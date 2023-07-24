1c17904950a983a772848f8527087e4d83d092bc
QingYang6 <Qing.Yang6@hotmail.com>
2023-07-24 17:54:26 +0300

    Create demo_cali.sh

---
+++ demo_project/demo_cali.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -p postproc# cluster
+#SBATCH --ntasks=3 
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-01:00:00
+cd /mnt/permanent/hydro/src/CREST_3.0
+srun -n3 -N1 -o /mnt/training/crest/qing/CREST_tutorial/demo_project/%j.out -l --multi-prog  /mnt/training/crest/qing/CREST_tutorial/demo_project/cali.conf
